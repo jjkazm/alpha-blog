c50d1f22a0d4f12d6fcd4df2a8c2896f4f510968
jjkazm <jjkazm@gmail.com>
2018-09-15 21:36:37 +0200

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_222912) do
+ActiveRecord::Schema.define(version: 2018_09_14_225748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 2018_09_13_222912) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "password_digest"
     t.integer "articles_count", default: 0
   end
