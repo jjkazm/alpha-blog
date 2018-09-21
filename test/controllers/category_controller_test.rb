7b07f8006bee92fdc109586073490a138a640185
jjkazm <jjkazm@gmail.com>
2018-09-21 23:47:17 +0200

    tests for admin requirements for categories

--- test/controllers/category_controller_test.rb
+++ test/controllers/category_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name:"news")
+    @user = User.create(username:"Wladek", email:"wlodek@wp.pl", password:"pass", admin:true)
   end
 
   test "should get categories index" do
@@ -11,13 +12,21 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get categories new" do
+    sign_in_as(@user, "pass")
     get new_category_path
     assert_response :success
   end
 
   test "should get categories show" do
-    get get show_category_path(@category)
+    get categories_path(@category)
     assert_response :success
+  end
+
+  test "should redirect if not logged as admin" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name: "news" }}
+    end
+    assert_redirected_to categories_path
   end
 
 
