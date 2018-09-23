80137a68a57453b42ab994a08f5f8f0f4a5e3942
jjkazm <jjkazm@gmail.com>
2018-09-23 22:32:12 +0200

    article_category_association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :user, counter_cache: true
   validates :title, presence: true, length: {minimum: 3, maximum: 80}
   validates :description, presence: true, length: {minimum: 5, maximum: 500}
