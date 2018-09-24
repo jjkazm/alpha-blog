087f8aa705a6500fb68183062d5541cab911ec0e
jjkazm <jjkazm@gmail.com>
2018-09-24 12:28:57 +0200

    add ArticleCategory data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# # add Users
-# User.create!(username: "Kuba", email: "jjkazm@gmail.com", password:"haslo", admin: true)
-# 5.times do
-#   User.create!(username: Faker::Internet.unique.username, email: Faker::Internet.unique.safe_email, password:"haslo")
-# end
-#
-#
-# # add Articles
-# 4.times do
-#   Article.create!(title: Faker::Coffee.unique.blend_name, description: Faker::Lebowski.unique.quote, user_id: 1)
-# end
-#
-# 10.times do
-#   Article.create!(title: Faker::Coffee.unique.blend_name, description: Faker::Lebowski.unique.quote, user_id: rand(2..6))
-# end
+# add Users
+User.create!(username: "Kuba", email: "jjkazm@gmail.com", password:"haslo", admin: true)
+5.times do
+  User.create!(username: Faker::Internet.unique.username, email: Faker::Internet.unique.safe_email, password:"haslo")
+end
+
+
+# add Articles
+4.times do
+  Article.create!(title: Faker::Coffee.unique.blend_name, description: Faker::Lebowski.unique.quote, user_id: 1)
+end
+
 10.times do
-  Category.create!(name: Faker::Color.unique.color_name)
+  Article.create!(title: Faker::Coffee.unique.blend_name, description: Faker::Lebowski.unique.quote, user_id: rand(2..6))
+end
+
+category = ["sports", "news", "politics", "environment", "travel", "programming", "tech", "animals", "books", "cooking"]
+category.each do |cat|
+  Category.create(name: cat)
+end
+
+i = 0
+while i < 20
+  art = rand(1..Article.count)
+  cat = rand(1..Category.count)
+  if !ArticleCategory.where(article_id: art, category_id: cat).exists?
+    ArticleCategory.create(article_id: art, category_id: cat)
+    i += 1
+  end
 end
